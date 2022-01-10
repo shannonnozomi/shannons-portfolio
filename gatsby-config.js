f18d75c56eddfa696924fdeba9acaf9bf7c8c18b
Shannon Nicholas <shannonnozomi@gmail.com>
2022-01-10 11:45:13 -0500

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Shannons Portfolio",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
 <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+      alt="Image of Shannon Nicholas"
+      src="../images/profilepic.png" />
     </Layout>
   )
 }
